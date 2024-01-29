8186776600cb02976f82d1d1f3f65d00d3524731
evanmusial <evan@musial.io>
2024-01-29 10:00:05 -0800

    More robust backup comments in manifest files

--- brew/brew-backup.sh
+++ brew/brew-backup.sh
@@ -6,11 +6,21 @@ cd ~/git/toolbox/brew/backups
 # Handy visual indicator that the backup is happening.
 echo "🦺 Backup of brew casks and formulae..."
 
+
+# An eyeball friendly date string, but use UTC for consistency.
+THIS_DATE=$(date -u +"%Y%m%d @ %H%M (UTC)")
+
+# Mark the files more handily.
+echo "# backup: $THIS_DATE" > brew-formulas.list.txt
+echo "# backup: $THIS_DATE" > brew-casks.list.txt
+echo "# backup: $THIS_DATE" > brew-casks.versions.txt
+echo "# backup: $THIS_DATE" > brew-formulas.versions.txt
+
 # Get list of brew formulas, and add that list to git.
-brew list --formula -1 > brew-formulas.list.txt
-brew list --cask -1 > brew-casks.list.txt
-brew list --cask --versions > brew-casks.versions.txt
-brew list --formula --versions > brew-formulas.versions.txt
+brew list --formula -1 >> brew-formulas.list.txt
+brew list --cask -1 >> brew-casks.list.txt
+brew list --cask --versions >> brew-casks.versions.txt
+brew list --formula --versions >> brew-formulas.versions.txt
 
 # Add the text files to a git commit, then push it.
 git add brew-formulas.list.txt
@@ -18,8 +28,6 @@ git add brew-casks.list.txt
 git add brew-casks.versions.txt
 git add brew-formulas.versions.txt
 
-# An eyeball friendly date string, but use UTC for consistency.
-THIS_DATE=$(date -u +"%Y%m%d @ %H%M (UTC)")
 
 # Make a git push with the date and time referenced, then push that to the remove repo.
 git commit -m "Brew formula & casks backup: ${THIS_DATE}"
